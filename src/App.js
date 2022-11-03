3b830ca3f650a0e9db6b224eb0683845e8e3f360
ASkauen <andreas@rubynor.com>
2022-11-03 15:33:22 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      task: {text: ""},
+      tasks: [],
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      task : {
+        text: e.target.value,
+      }
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: { text: '' },
+    });
+  };
+
+  render(){
+    const {task, tasks} = this.state
+    return(
+        <div>
+          <form onSubmit={this.onSubmitTask}>
+            <label htmlFor="taskInput">Enter task</label>
+            <input onChange={this.handleChange} value={task.text} type="text" id="taskInput"/>
+            <button type="submit">
+              Add Task
+            </button>
+          </form>
+        </div>
+    )
+  }
+
 }
 
 export default App;
